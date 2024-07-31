ad197bf6f63ede4ce93739d54a07369e936a07de
DESKTOP-1D3SA0A\user <kdhbd98@yu.ac.kr>
2024-07-31 17:43:42 +0900

    240731

--- html_work/audio(1)/javascript.js
+++ html_work/audio(1)/javascript.js
@@ -1,3 +1,8 @@
-$("h1").html(function(index, html) {
-    return html.replace(/\S/g, '<span>$&</span>');
-  });
+// Optional JavaScript to enhance the glitch effect
+// Adding randomness to the glitch effect
+
+const glitchText = document.querySelector('.glitch');
+
+setInterval(() => {
+  glitchText.style.transform = `translate(${Math.random() * 10 - 5}px, ${Math.random() * 10 - 5}px)`;
+}, 100);
