86c77c8b779fa9b70c439fd27a7d18d15410510c
DESKTOP-1D3SA0A\user <kdhbd98@yu.ac.kr>
2024-08-14 15:48:06 +0900

    no message

--- java_work/ex24/src/main/java/org/example/DB/DBMain.java
+++ java_work/ex24/src/main/java/org/example/DB/DBMain.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 public class DBMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        DBRepository dbRepository = new DBRepository();
         while (true){
             System.out.println("""
                     Which one will you choose?
-                    1. enter
-                    2. research
-                    3. modify
+                    1. insert
+                    2. select
+                    3. update
                     4. delete
                     5. exit
                     """);
             int cho = scanner.nextInt();
             if (cho == 1){
-
-            } else if (cho == 2) {
-
+                dbRepository.insert();
+            }
+            else if (cho == 2) {
+                dbRepository.select();
             }
             else {
                 System.out.println("exit.");
--- java_work/ex24/src/main/java/org/example/DB/DBRepository.java
+++ java_work/ex24/src/main/java/org/example/DB/DBRepository.java
@@ -1,7 +1,27 @@
 package org.example.DB;
 
-public class DBRepository {
-    public void insert(){
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBRepository {
+    public void insert() {
+        try {
+
+//          1. mysql jar 추가 확인
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+//          2. connection 연결
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/new", "root", "1234");
+            System.out.println("success");
+
+//          3. sql 문 작성
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Come on.");
+    }
+
+    public void select() {
+        System.out.println("Select.");
     }
 }
