3a759a255a82e765f5349d95848a031d53b52dc5
DESKTOP-1D3SA0A\user <kdhbd98@yu.ac.kr>
2024-08-14 15:13:54 +0900

    no message

--- java_work/ex24/src/main/java/org/example/DB/DBMain.java
+++ java_work/ex24/src/main/java/org/example/DB/DBMain.java
@@ -1,7 +1,29 @@
 package org.example.DB;
 
+import java.util.Scanner;
+
 public class DBMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("""
+                    Which one will you choose?
+                    1. enter
+                    2. research
+                    3. modify
+                    4. delete
+                    5. exit
+                    """);
+            int cho = scanner.nextInt();
+            if (cho == 1){
 
+            } else if (cho == 2) {
+
+            }
+            else {
+                System.out.println("exit.");
+                break;
+            }
+        }
     }
 }
